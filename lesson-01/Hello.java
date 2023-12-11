efc407420e0880f775587b1abd81b053534d348c
Ben Johnson <monk.sound.design@gmail.com>
2023-12-11 12:54:45 -0500

    data types

--- lesson-01/Hello.java
+++ lesson-01/Hello.java
@@ -2,10 +2,18 @@ class Hello
 {
     public static void main(String a[]) 
     {
-        int num1 = 3;
-        int num2 = 7;
-        int result = num1 + num2;
-        System.out.println(result);
+       int num1 = 9;
+       byte by = 127;
+       short sh = 558;
+       long l = 5854;
+
+       float f = 5.8f;
+       double d = 5.8;
+
+       char c = 'k';
+
+       boolean bool = true;
+        // System.out.println(isActive);
     }
 }
 
