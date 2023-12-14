e54d13b87dd75f51909389ecb1f79d9e61237ece
Ben Johnson <monk.sound.design@gmail.com>
2023-12-14 11:48:53 -0500

    do while loop

--- lesson-03/Loops.java
+++ lesson-03/Loops.java
@@ -4,23 +4,18 @@ class Loops
     {
         // loops: while, do while, for\
         // number of times OR under certain condition
-        int i = 1;
-        while(i <= 4)
+        int i = 5;
+
+        do
         {
             System.out.println("Hi" + i);
             
-            int j = 1;
-            while(j <= 3)
-            {
-                System.out.println("Hello" + j);
-                j++;
-            }
-
             i++;
         }
+        while(i <= 4);
 
 
-        System.out.println("Bye" + i);
+        // System.out.println("Bye" + i);
     }
     
 }
