723f23e3271d68d603d8f3c4160fb7afd9d52cb5
Ben Johnson <monk.sound.design@gmail.com>
2023-12-14 10:36:30 -0500

    conditions

---
+++ lesson-02/Conditions.java
@@ -0,0 +1,18 @@
+class Conditions 
+{
+    public static void main(String args[]) 
+    {
+
+        int x = 8;
+        int y = 10;
+
+        if(x > y) {
+            System.out.println(x);
+
+        } 
+        else {
+            
+            System.out.println(y);
+        }
+    }
+}
