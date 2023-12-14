afc1400bdba85b456f5753693eb9dccb3da91cf2
Ben Johnson <monk.sound.design@gmail.com>
2023-12-14 15:05:32 -0500

    stack memory, heap memory, onject references

--- lesson-05/Methods.java
+++ lesson-05/Methods.java
@@ -1,12 +1,11 @@
 class Calculator
 {
-    public int add(int n1, int n2, int n3)
-    {
-        return n1 + n2 + n3;
-    }
+
+    int num = 5;
 
     public int add(int n1, int n2)
     {
+        // System.out.println(num);
         return n1 + n2;
     }
 }
@@ -14,9 +13,15 @@ class Calculator
 public class Methods {
     public static void main(String a[])
     {
+        int data = 10;
         Calculator obj = new Calculator();
+        Calculator obj1 = new Calculator();
+
+        obj.num = 8;
 
         int r1 = obj.add(3, 4);
-        System.out.println(r1);
+        // System.out.println(r1);
+        System.out.println(obj.num);
+        System.out.println(obj1.num);
     }
 }
