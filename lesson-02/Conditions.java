9738d341c69c3a65dcc512fb6660c151c1c3ab1f
Ben Johnson <monk.sound.design@gmail.com>
2023-12-14 10:46:06 -0500

    switch, case, break

--- lesson-02/Conditions.java
+++ lesson-02/Conditions.java
@@ -3,10 +3,38 @@ class Conditions
     public static void main(String args[]) 
     {
 
-        int n = 4;
+        int n = 5;
 
-        boolean result = n != 4 ? true : false;
+        switch(n)
+        {
+            case 1: 
+            System.out.println("Monday");
+            break;
 
-        System.out.println(result);
+            case 2: 
+            System.out.println("Tuesday");
+            break;
+            
+            case 3: 
+            System.out.println("Wednesday");
+            break;
+            
+            case 4: 
+            System.out.println("Thursday");
+            break;
+            
+            case 5: 
+            System.out.println("Friday");
+            break;
+            
+            case 6: 
+            System.out.println("Saturday");
+            break;
+            
+            case 7: 
+            System.out.println("Sunday");
+            break;
+           
+        }
     }
 }
