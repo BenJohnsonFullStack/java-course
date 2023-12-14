0f5ee69cf76a7ecef59a9d3d8f8ff00bb72c3181
Ben Johnson <monk.sound.design@gmail.com>
2023-12-14 10:54:01 -0500

    while loop

---
+++ lesson-03/Loops.java
@@ -0,0 +1,18 @@
+class Loops 
+{
+    public static void main(String args[])
+    {
+        // loops: while, do while, for\
+        // number of times OR under certain condition
+        int count = 0;
+        while(count < 4)
+        {
+            System.out.println("Hi");
+            count++;                
+        }
+
+
+
+    }
+    
+}
