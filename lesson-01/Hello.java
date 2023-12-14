59dcf4f9ac4385aa90b647d5f726d3dd88271cd6
Ben Johnson <monk.sound.design@gmail.com>
2023-12-14 10:05:20 -0500

    conversion and increment

--- lesson-01/Hello.java
+++ lesson-01/Hello.java
@@ -1,9 +1,19 @@
 class Hello
 {
-    public static void main(String a[]) 
+    public static void main(String args[]) 
     {
-        byte num1 = 10;
-        System.out.println(num1);
+       int num = 7;
+    //    int num2 = 5;
+
+    //    float result = (float) num1 / num2;
+
+    // num++     // post-increment
+    // ++num;    // pre-increment
+
+    int result = num++; // fetch the value, then increment (two statements)
+    // int result = ++num  // increment, then fetch value 
+
+        System.out.println(result);
     }
 }
 
