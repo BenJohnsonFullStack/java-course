a8fe6c22c622c11808d7a086f1cc5f68415e24e8
Ben Johnson <monk.sound.design@gmail.com>
2023-12-14 19:42:09 -0500

    static keyword

---
+++ lesson-09/Static.java
@@ -0,0 +1,29 @@
+class Mobile
+{
+    String brand;
+    static String name;
+    int price;
+
+    public void show()
+    {
+        System.out.println(brand + " : " + price + " : " + name);
+    }
+}
+
+class Static {
+    public static void main(String a[])
+    {
+        Mobile obj1 = new Mobile();
+        obj1.brand = "Apple";
+        obj1.price = 1500;
+        
+        Mobile obj2 = new Mobile();
+        obj2.brand = "Samsung";
+        obj2.price = 1700;
+
+        Mobile.name = "Phone"; // static name reference
+
+        obj1.show();
+        obj2.show();
+    }
+}
