0ecd6743e01dc9278486e58182b9c25d6c3ad79b
Ben Johnson <monk.sound.design@gmail.com>
2023-12-14 19:15:56 -0500

    strings and concat

---
+++ lesson-08/Strings.java
@@ -0,0 +1,12 @@
+// navin
+
+class Strings {
+    public static void main(String a[])
+    {
+        String name = "Navin";
+
+        System.out.println("Hello, " + name);
+        System.out.println(name.concat(" Reddy"));
+        
+    }
+}
