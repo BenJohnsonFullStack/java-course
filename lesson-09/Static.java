2989e48ab4716d1a8e1111389e596089c66438ff
Ben Johnson <monk.sound.design@gmail.com>
2023-12-14 21:44:41 -0500

    static variables

--- lesson-09/Static.java
+++ lesson-09/Static.java
@@ -4,6 +4,19 @@ class Mobile
     static String name;
     int price;
 
+    static
+    {
+        name = "Phone";
+        System.out.println("in static block");
+    }
+
+    public Mobile()
+    {
+        brand = "";
+        price = 200;
+        System.out.println("in constructor");
+    }
+
     public void show()
     {
         System.out.println(brand + " : " + price + " : " + name);
@@ -11,19 +24,22 @@ class Mobile
 }
 
 class Static {
-    public static void main(String a[])
+    public static void main(String a[]) throws ClassNotFoundException
     {
-        Mobile obj1 = new Mobile();
-        obj1.brand = "Apple";
-        obj1.price = 1500;
+
+        Class.forName("Mobile");
+
+        // Mobile obj1 = new Mobile();
+        // obj1.brand = "Apple";
+        // obj1.price = 1500;
         
-        Mobile obj2 = new Mobile();
-        obj2.brand = "Samsung";
-        obj2.price = 1700;
+        // Mobile obj2 = new Mobile();
+        // obj2.brand = "Samsung";
+        // obj2.price = 1700;
 
-        Mobile.name = "Phone"; // static name reference
+        // Mobile.name = "Phone"; // static name reference
 
-        obj1.show();
-        obj2.show();
+        // obj1.show();
+        // obj2.show();
     }
 }
