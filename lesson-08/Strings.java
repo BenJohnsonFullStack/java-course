53badabbf3c8aab90b394752ce55126afbbc0f54
Ben Johnson <monk.sound.design@gmail.com>
2023-12-14 19:22:35 -0500

    immutability in strings

--- lesson-08/Strings.java
+++ lesson-08/Strings.java
@@ -4,9 +4,13 @@ class Strings {
     public static void main(String a[])
     {
         String name = "Navin";
+        name = name + " Reddy";
 
         System.out.println("Hello, " + name);
-        System.out.println(name.concat(" Reddy"));
-        
+
+        String s1 = "Navin";
+        String s2 = "Navin";
+
+        System.out.println(s1 == s2);
     }
 }
