90e1d793b48503a194f4206e6190c16f26d07fbb
Ben Johnson <monk.sound.design@gmail.com>
2023-12-14 21:53:30 -0500

    static methods

--- lesson-09/Static.java
+++ lesson-09/Static.java
@@ -4,42 +4,36 @@ class Mobile
     static String name;
     int price;
 
-    static
-    {
-        name = "Phone";
-        System.out.println("in static block");
-    }
-
-    public Mobile()
-    {
-        brand = "";
-        price = 200;
-        System.out.println("in constructor");
-    }
-
     public void show()
     {
         System.out.println(brand + " : " + price + " : " + name);
     }
+
+    public static void show1(Mobile obj)
+    {
+        System.out.println(obj.brand + " : " + obj.price + " : " + name);
+    }
 }
 
 class Static {
-    public static void main(String a[]) throws ClassNotFoundException
+    public static void main(String a[])
     {
 
-        Class.forName("Mobile");
-
-        // Mobile obj1 = new Mobile();
-        // obj1.brand = "Apple";
-        // obj1.price = 1500;
         
-        // Mobile obj2 = new Mobile();
-        // obj2.brand = "Samsung";
-        // obj2.price = 1700;
 
-        // Mobile.name = "Phone"; // static name reference
+        Mobile obj1 = new Mobile();
+        obj1.brand = "Apple";
+        obj1.price = 1500;
+        
+        Mobile obj2 = new Mobile();
+        obj2.brand = "Samsung";
+        obj2.price = 1700;
 
-        // obj1.show();
-        // obj2.show();
+        Mobile.name = "Phone"; // static name reference
+
+        obj1.show();
+        obj2.show();
+
+        Mobile.show1(obj1);
     }
 }
